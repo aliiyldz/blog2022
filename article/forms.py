9d81964a5f5356c159b557c3cefe3417d81203f9
aliyldz <aliyldzim@gmail.com>
2022-10-09 10:57:14 +0300

    Dosyalar Localden Yüklendi

---
+++ article/forms.py
@@ -0,0 +1,13 @@
+from dataclasses import field
+from django import forms
+from .models import Article
+
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ["title","content","article_image"]
+
+
+
+
